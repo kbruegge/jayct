bef859d56e5ebfebd2e8c29d2afd77f0d017c091
Alexey Egorov <alexey.egorov@tu-dortmund.de>
2018-04-13 14:10:07 +0200

    use setup and teardown methods for testing python bridge (with new start method)

--- src/test/java/PythonTest.java
+++ src/test/java/PythonTest.java
@@ -1,7 +1,10 @@
 import com.google.common.collect.ImmutableMap;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
@@ -10,35 +13,46 @@ import pythonbridge.PythonBridge;
 import reconstruction.containers.Moments;
 
 public class PythonTest {
+    static PythonBridge bridge;
+
+    @Before
+    public void setUp() throws Exception {
+        // get the python bridge
+        bridge = PythonBridge.getInstance();
+        bridge.start();
+        assert bridge != null;
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // close the python bridge
+        bridge.close();
+    }
 
     @Test
     public void testBridge() throws Exception {
-        PythonBridge bridge = PythonBridge.getInstance();
-
-        assert bridge != null;
-
         String s1 = "Hello! I pitty the fool.";
         String s2 = (String) bridge.callMethod("ping", s1);
 
         assert s1.equals(s2);
-
-        bridge.close();
     }
 
 
     @Test
     public void testHillas() throws Exception {
-        PythonBridge bridge = PythonBridge.getInstance();
         Random r = new Random();
-        assert bridge != null;
 
         double[] b = new double[1855];
         for (int i = 0; i < b.length; i++) {
             b[i] = r.nextDouble() * 50;
         }
 
-        HashMap<String, Double> o = (HashMap<String, Double>) bridge.callMethod("hillas", ImmutableMap.of("image", b, "cameraId", 1, "eventId", 5));
-        bridge.close();
+        ImmutableMap<String, Serializable> input = ImmutableMap.of(
+                "image", b, "cameraId", 1, "eventId", 5);
+
+        // call python script
+        @SuppressWarnings("unchecked")
+        HashMap<String, Double> o = (HashMap<String, Double>) bridge.callMethod("hillas", input);
 
         Moments m = new Moments(1, 1, 2, 2,
                 o.get("width"),
@@ -58,23 +72,22 @@ public class PythonTest {
 
     @Test
     public void testTailCut() throws Exception {
-        PythonBridge bridge = PythonBridge.getInstance();
-
         Random r = new Random();
-        assert bridge != null;
 
         double[] b = new double[1855];
         for (int i = 0; i < b.length; i++) {
             b[i] = r.nextDouble() * 50;
         }
-        Object[] result = (Object[]) bridge.callMethod("tail_cut", ImmutableMap.of("image", b, "cameraId", 1, "eventId", 5));
+
+        ImmutableMap<String, Serializable> input = ImmutableMap.of(
+                "image", b, "cameraId", 1, "eventId", 5);
+        Object[] result = (Object[]) bridge.callMethod("tail_cut", input);
 
         // cast the results into array
         @SuppressWarnings("unchecked")
         ArrayList<Integer> index = (ArrayList<Integer>) result[0];
+
         @SuppressWarnings("unchecked")
         ArrayList<Double> weights = (ArrayList<Double>) result[1];
-
-        bridge.close();
     }
 }
