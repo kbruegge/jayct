29be89cf3ca48ed939e1895f332f1bcb49b475d8
Alexey Egorov <alexey.egorov@tu-dortmund.de>
2018-04-13 13:31:28 +0200

    apply PEP8 style

--- src/main/resources/python/reconstruct_direction.py
+++ src/main/resources/python/reconstruct_direction.py
@@ -25,12 +25,13 @@ SubMomentParameters = namedtuple('SubMomentParameters', 'size,cen_x,cen_y,length
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, "../instrument_description.pkl")
 
+
 def dummy_function_h_max(self, hillas_dict, subarray, tel_phi):
     return -1
 
+
 @Pyro4.expose
 class Reconstructor():
-
     instrument = pickle.load(open(filename, 'rb'))
 
     def reconstruct_direction(self, moments):
@@ -41,26 +42,29 @@ class Reconstructor():
         params = {}
         pointing_azimuth = {}
         pointing_altitude = {}
-        #TODO: what about array event id?
+        # TODO: what about array event id?
         for row in moments:
             tel_id = row["telescopeID"]
             # the data in each event has to be put inside these namedtuples to call reco.predict
-            #TODO: what is psi? phi or delta?
+            # TODO: what is psi? phi or delta?
             moment = SubMomentParameters(size=row["size"], cen_x=row["meanX"] * units.m,
-                                          cen_y=row["meanY"] * units.m, length=row["length"] * units.m,
-                                          width=row["width"] * units.m, psi=row["phi"] * units.deg)
+                                         cen_y=row["meanY"] * units.m,
+                                         length=row["length"] * units.m,
+                                         width=row["width"] * units.m, psi=row["phi"] * units.deg)
             params[tel_id] = moment
-            pointing_azimuth[tel_id] = 0 * units.rad # row.pointing_azimuth * units.rad
-            pointing_altitude[tel_id] = 1.2217304763960306 * units.rad #row.pointing_altitude * units.rad
+            pointing_azimuth[tel_id] = 0 * units.rad  # row.pointing_azimuth * units.rad
+            pointing_altitude[
+                tel_id] = 1.2217304763960306 * units.rad  # row.pointing_altitude * units.rad
 
         try:
-            reconstruction = reco.predict(params, self.instrument, pointing_azimuth, pointing_altitude)
+            reconstruction = reco.predict(params, self.instrument, pointing_azimuth,
+                                          pointing_altitude)
         except NameError:
             return {'alt_prediction': np.nan,
                     'az_prediction': np.nan,
                     'core_x_prediction': np.nan,
                     'core_y_prediction': np.nan,
-                    #'array_event_id': array_event_id,
+                    # 'array_event_id': array_event_id,
                     }
 
         if reconstruction.alt.si.value == np.nan:
@@ -72,7 +76,7 @@ class Reconstructor():
                 'az_prediction': reconstruction.az.si.value,
                 'core_x_prediction': reconstruction.core_x.si.value,
                 'core_y_prediction': reconstruction.core_y.si.value,
-                #'array_event_id': array_event_id,
+                # 'array_event_id': array_event_id,
                 # 'h_max_prediction': reconstruction.h_max.si.value
                 }
 
@@ -95,7 +99,6 @@ class Reconstructor():
             print("TAILCUT ERROR:")
             print(traceback.format_exc())
             result = list(np.zeros(1)), list(np.zeros(1))
-            print(result)
             return result
 
     def hillas(self, input):
@@ -107,33 +110,35 @@ class Reconstructor():
             image = np.array(image)
             hillas = hillas_parameters(geom, image)
 
-            d = {'size':hillas.size,
-                 'width':hillas.width.value,
-                 'length':hillas.length.value,
-                 'cen_x':hillas.cen_x.value,
-                 'cen_y':hillas.cen_y.value,
-                 'r':hillas.r.value,
-                 'psi':hillas.psi.value,
-                 'phi':hillas.phi.value,
-                 'skewness':hillas.skewness,
-                 'kurtosis':hillas.kurtosis,
-                 'miss':hillas.miss.value,
+            d = {'size': hillas.size,
+                 'width': hillas.width.value,
+                 'length': hillas.length.value,
+                 'cen_x': hillas.cen_x.value,
+                 'cen_y': hillas.cen_y.value,
+                 'r': hillas.r.value,
+                 'psi': hillas.psi.value,
+                 'phi': hillas.phi.value,
+                 'skewness': hillas.skewness,
+                 'kurtosis': hillas.kurtosis,
+                 'miss': hillas.miss.value,
                  'eventId': input["eventId"],
                  'cameraId': input["cameraId"]
                  }
             return d
-        except Exception as e:
-            print("FEHLER")
+        except:
+            print("HILLAS PARAMETRIZATION EXCEPTION")
             print(traceback.format_exc())
-            return {'size':np.nan,
-                    'width':np.nan,
-                    'length':np.nan,
-                    'cen_x':np.nan,
-                    'cen_y':np.nan,
-                    'r':np.nan,
-                    'psi':np.nan,
-                    'phi':np.nan,
-                    'skewness':np.nan,
-                    'kurtosis':np.nan,
-                    'miss':np.nan, 'eventId': input["eventId"], 'cameraId': input["cameraId"]
+            return {'size': np.nan,
+                    'width': np.nan,
+                    'length': np.nan,
+                    'cen_x': np.nan,
+                    'cen_y': np.nan,
+                    'r': np.nan,
+                    'psi': np.nan,
+                    'phi': np.nan,
+                    'skewness': np.nan,
+                    'kurtosis': np.nan,
+                    'miss': np.nan,
+                    'eventId': input["eventId"],
+                    'cameraId': input["cameraId"]
                     }
